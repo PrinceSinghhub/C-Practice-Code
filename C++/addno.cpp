8c1bd59f74985a5190bb2ff67d794af9c004d1d9
Prince Singh <thisisprincesingh@gmail.com>
2023-01-27 19:49:02 +0530

    Add files via upload
    Add all files

---
+++ C++/addno.cpp
@@ -0,0 +1,11 @@
+# include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"num1: ";
+    cin>>num1;
+    cout<<"num2: ";
+    cin>>num2;
+    cout<<"the sum is: "<<num1+num2;
+    return 0;
+}
---
+++ C++/mull.cpp
@@ -0,0 +1,16 @@
+# include<iostream>
+using namespace std;
+int main(){
+    int num1,num2;
+    cout<<"num1:\n";
+    cin>>num1;
+    cout<<"num2:\n";
+    cin>>num2;
+
+    cout<<"the multiplication is"<<num1*num2;
+    
+
+    return 0;
+
+
+}
